7a4c4bffb1d989eb5234ceaa99900cfffda28f66
vineetyadav2403 <46716473+vineetyadav2403@users.noreply.github.com>
2020-11-07 23:43:57 +0530

    Update lcm_goup_query.cpp

--- SegmentTree/lcm_goup_query.cpp
+++ SegmentTree/lcm_goup_query.cpp
@@ -44,6 +44,9 @@ int query(int node, int start, int end, int l, int r){
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int t;
     cin>>t;
     while(t--){
