628cb3513de9e239509004c53cdb4247158e3e7c
vineetyadav2403 <46716473+vineetyadav2403@users.noreply.github.com>
2020-12-25 17:08:01 +0530

    Update word break 139.cpp

--- Leetcode/word break 139.cpp
+++ Leetcode/word break 139.cpp
@@ -18,3 +18,26 @@ bool isWord(string s,vector<string>& wordDict){
         }
         return false;
     }
+
+// Memorization solution
+unordered_map<string,bool> mp;
+    bool isWord(string s,vector<string>& wordDict){
+        for(auto i:wordDict){
+            if(i==s) return true;
+        }
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto i:wordDict){
+            if(i==s) return true;
+        }
+        if(mp.find(s)!=mp.end()) return mp[s];
+        for(int i=1;i<s.length();i++){
+            if(isWord(s.substr(0,i),wordDict)&&wordBreak(s.substr(i),wordDict)){
+                mp[s] = true;
+                return true;
+            }
+        }
+        mp[s] = false;
+        return false;
+    }
