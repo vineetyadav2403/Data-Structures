780c2470146a9c0e242ccb944c9a08d223562c89
vineetyadav2403 <46716473+vineetyadav2403@users.noreply.github.com>
2020-12-28 17:12:59 +0530

    Create findPathSum_437.cpp

---
+++ Tree/findPathSum_437.cpp
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/path-sum-iii/
+
+int total = 0;
+    void findPath(TreeNode *root,int &sum, int curr){
+        if(!root) return;
+        curr+=root->val;
+        if(curr==sum)
+            total++;
+        findPath(root->left,sum,curr);
+        findPath(root->right,sum,curr);
+    }
+    
+    
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        findPath(root,sum,0);
+        pathSum(root->left,sum);
+        pathSum(root->right,sum);
+        
+        return total;
+    }
