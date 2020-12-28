de97e6d860ea434bb3d1a7af2381e273af74786c
vineetyadav2403 <46716473+vineetyadav2403@users.noreply.github.com>
2020-12-28 18:24:55 +0530

    Create equal sum partition sum 416.cpp

---
+++ dp/equal sum partition sum 416.cpp
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/partition-equal-subset-sum/
+
+bool canPartition(vector<int>& nums) {
+        int i,sum = 0,j,n=nums.size();
+        for(i=0;i<n;i++) sum+=nums[i];
+        if(sum%2!=0){
+            return false;
+        }else{
+            sum /= 2;
+            vector<vector<bool>> dp(n+1,vector<bool>(sum+1,false));
+            for(i=0;i<=n;i++){
+                dp[i][0] = true;
+            }
+            for(j=0;j<=sum;j++){
+                dp[0][j] = false;
+            }
+            for(i = 1;i<=n;i++){
+                for(j=1;j<=sum;j++){
+                    if(nums[i-1]<=j){
+                        dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                    }else{
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+            }
+            return dp[n][sum];
+        }
+    }
