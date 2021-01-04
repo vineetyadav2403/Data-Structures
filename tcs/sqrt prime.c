67ea638a294d41f7c1f46e27e5c1c453f447032d
vineetyadav2403 <46716473+vineetyadav2403@users.noreply.github.com>
2021-01-04 22:30:48 +0530

    Update sqrt prime.c

--- tcs/sqrt prime.c
+++ tcs/sqrt prime.c
@@ -1,3 +1,28 @@
+//******************
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
+{
+ if(argc==1)
+ {
+ printf("No arguments");
+ return 0;
+ }
+ else
+ {
+ int n;
+ n=atoi(argv[1]);
+ float i=0.00;
+ while(i*i<=n)
+ {
+ i=i+0.001;
+ }
+ i=i-0.001;
+ printf("%.2f",i);
+ }
+}
+//**************
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
